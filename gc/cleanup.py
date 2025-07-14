73bceb4d30cd124c4d3cc213e0e554cebfb2dc4c
Alyssa Holland <2011hollanda@gmail.com>
2025-07-14 04:09:34 +0000

    Minor Fixes

--- gc/cleanup.py
+++ gc/cleanup.py
@@ -5,7 +5,7 @@ def cleanup():
     immich_tiktok_pods = os.popen("kubectl get pods -n immich-tiktok-remover-api --no-headers | awk '{print $1}'").read()
 
     for pod in immich_tiktok_pods.splitlines():
-        if "Invalid API key" in os.popen("kubectl logs {} --tail 12 -n immich-tiktok-remover-api".format(pod)).read():
+        if ("Invalid API key" in os.popen("kubectl logs {} --tail 12 -n immich-tiktok-remover-api".format(pod)).read()) or ("immich_end" in os.popen("kubectl logs {} --tail 12 -n immich-tiktok-remover-api".format(pod)).read()):
             os.popen("kubectl delete deployment {} -n immich-tiktok-remover-api".format(pod[:-9]))
             print("Deleted deployment {}".format(pod[:-9]))
         else:
