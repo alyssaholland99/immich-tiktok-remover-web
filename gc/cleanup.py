6c7a5010620474bc49d18bdb19a19f09f93085c3
Alyssa Holland <hollan84@coventry.ac.uk>
2025-07-15 16:09:03 +0100

    delete release secrets

--- gc/cleanup.py
+++ gc/cleanup.py
@@ -7,10 +7,14 @@ def cleanup():
     for pod in immich_tiktok_pods.splitlines():
         if ("Invalid API key" in os.popen("kubectl logs {} --tail 12 -n immich-tiktok-remover-api".format(pod)).read()) or ("immich_end" in os.popen("kubectl logs {} --tail 12 -n immich-tiktok-remover-api".format(pod)).read()):
             deployment_name = pod.split("-")[0] + "-" + pod.split("-")[1]
-            os.popen("kubectl delete deployment {} -n immich-tiktok-remover-api".format(deployment_name))
+            os.popen("kubectl delete deployment {} -n immich-tiktok-remover-api".format(deployment_name)).read()
             print("Deleted deployment {}".format(deployment_name))
-            os.popen("kubectl delete secret {}-auth -n immich-tiktok-remover-api".format(deployment_name.replace("itr", "immich-tiktok-remover")))
+            os.popen("kubectl delete secret {}-auth -n immich-tiktok-remover-api".format(deployment_name.replace("itr", "immich-tiktok-remover"))).read()
             print("Deleted secret for {}".format(deployment_name))
+            release_secrets = os.popen("kubectl get secrets -n immich-tiktok-remover-api | grep {}".format(deployment_name)).read()
+            for secret in release_secrets.splitlines():
+                os.popen("kubectl delete secret {} -n immich-tiktok-remover-api".format(secret))
+                print("Deleted {} for {}".format(secret, deployment_name))
         else:
             print("{} is still running".format(pod))
 
