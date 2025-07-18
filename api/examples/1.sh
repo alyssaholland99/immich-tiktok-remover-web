b24ac8c7ddfe604c2007ab5bd86ca1b3ea7db86e
Alyssa Holland <hollan84@coventry.ac.uk>
2025-07-18 19:53:35 +0100

    More cleanup

--- api/examples/1.sh
+++ api/examples/1.sh
@@ -1,9 +1,5 @@
 curl -X POST -H "Content-Type: application/json" -d '{
-  "immich_url": "https://immich.alyssaserver.co.uk/",
-  "immich_api_key": "WHablZwyi9OfhsUcMiZXeisQD3OISzVc0KIX0O54",
+  "immich_url": "https://immich.yourserver.co.uk/",
+  "immich_api_key": "dfadfgadfgasdfdfasadfsasdfasdf",
   "repeat": true
 }' https://api.immich-tiktok-remover.co.uk/remove
-
-curl -X GET -H "Content-Type: application/json" -d '{
-  "pod_id": "immich-tiktok-remover-ae7b5af6274808ba401bfc4538d3a97f-9fbf7qtn"
-}' http://localhost:5000/logs
